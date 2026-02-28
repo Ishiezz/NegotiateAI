653f32e1c58013b168aebac78ec6c4f10ff8fdc6
Ishiezz <singhishaa.24@gmail.com>
2026-02-28 14:15:00 +0530

    refactor: migrate database schema to Prisma

--- backend/src/models/RFQ.model.ts
+++
@@ -1,7 +0,0 @@
-export interface RFQDocument {
-  buyerId: string;
-  category: string;
-  quantity: number;
-}
-
-export const RFQModelPurpose = "Request-for-quote storage with quote sub-doc support.";
